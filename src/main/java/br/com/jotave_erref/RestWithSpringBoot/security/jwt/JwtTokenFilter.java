8674098bf44aac0be3f1f03be0410223ec86366d
Jean Victor <jvrferreira96@gmail.com>
2024-08-22 15:41:35 -0300

    Implements JWT Configurer and Token Filter

--- src/main/java/br/com/jotave_erref/RestWithSpringBoot/security/jwt/JwtTokenFilter.java
+++ src/main/java/br/com/jotave_erref/RestWithSpringBoot/security/jwt/JwtTokenFilter.java
@@ -1,4 +1,32 @@
 package br.com.jotave_erref.RestWithSpringBoot.security.jwt;
 
-public class JwtTokenFilter {
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.io.IOException;
+
+public class JwtTokenFilter extends GenericFilter{
+
+    @Autowired
+    private JwtTokenProvider tokenProvider;
+
+    public JwtTokenFilter(JwtTokenProvider tokenProvider) {
+        this.tokenProvider = tokenProvider;
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        String token = tokenProvider.resolveToken((HttpServletRequest) request);
+        if (token != null && tokenProvider.validateToken(token)) {
+            Authentication auth = tokenProvider.getAuthentication(token);
+            if (auth != null) {
+                SecurityContextHolder.getContext().setAuthentication(auth);
+            }
+        }
+        chain.doFilter(request, response);
+    }
 }
